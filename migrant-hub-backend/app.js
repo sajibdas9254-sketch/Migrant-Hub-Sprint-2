adc95da71d579ea9cf3b9e1f874a17256f8bd078
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-14 23:13:31 +0300

    Add posts route

--- migrant-hub-backend/app.js
+++ migrant-hub-backend/app.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+const postRoutes = require("./routes/postRoutes");
+
+app.use("/api/posts", postRoutes);
+
 const PORT = 4000;
 
 app.listen(PORT, () => {
