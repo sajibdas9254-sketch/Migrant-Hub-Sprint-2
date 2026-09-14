a480dfd5fad29ec139db673f958db8041d06752b
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-15 00:19:42 +0300

    Complete posts CRUD routes

--- migrant-hub-backend/controllers/postController.js
+++ migrant-hub-backend/controllers/postController.js
@@ -2,6 +2,29 @@ const getAllPosts = (req, res) => {
   res.json([]);
 };
 
+const getPostById = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const createPost = (req, res) => {
+  res.status(201).json(req.body);
+};
+
+const updatePost = (req, res) => {
+  res.json({
+    id: req.params.id,
+    ...req.body
+  });
+};
+
+const deletePost = (req, res) => {
+  res.status(204).send();
+};
+
 module.exports = {
-  getAllPosts
+  getAllPosts,
+  getPostById,
+  createPost,
+  updatePost,
+  deletePost
 };
--- migrant-hub-backend/routes/postRoutes.js
+++ migrant-hub-backend/routes/postRoutes.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getAllPosts } = require("../controllers/postController");
+const {
+  getAllPosts,
+  getPostById,
+  createPost,
+  updatePost,
+  deletePost
+} = require("../controllers/postController");
 
 router.get("/", getAllPosts);
+router.get("/:id", getPostById);
+router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 module.exports = router;
