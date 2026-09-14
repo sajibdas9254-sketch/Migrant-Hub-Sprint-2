fe642e2f8136ee7b6dfaffa32ce6aa7d691540d7
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-14 23:27:39 +0300

    Add Morgan request logger

--- migrant-hub-backend/app.js
+++ migrant-hub-backend/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan("dev"));
 
 const postRoutes = require("./routes/postRoutes");
 
