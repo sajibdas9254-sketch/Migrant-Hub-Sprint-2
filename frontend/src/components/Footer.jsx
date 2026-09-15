75ce001eec7954a5f0a46f8e3b5c9a240926031c
Prabhleen kaur <prabhleenkaur9946@gmail.com>
2026-09-15 14:19:12 +0300

    feat: add shared navbar and footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+    return (
+        <footer className="site-footer">
+            <div className="footer-content">
+                <h3>Migrant Hub</h3>
+                <p>
+                    Helping international students and newcomers settle into life in
+                    Finland.
+                </p>
+
+                <p className="copyright">
+                    © 2026 Migrant Hub
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <header className="site-header">
+            <nav className="navbar">
+                <NavLink to="/" className="logo">
+                    Migrant Hub
+                </NavLink>
+
+                <div className="nav-links">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/blog">Blog</NavLink>
+                    <NavLink to="/must-do">Must Do</NavLink>
+                    <NavLink to="/community">Community</NavLink>
+                    <NavLink to="/search">Search</NavLink>
+                    <NavLink to="/login">Login</NavLink>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
