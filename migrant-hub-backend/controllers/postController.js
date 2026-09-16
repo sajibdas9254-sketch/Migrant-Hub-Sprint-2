f99040789e4acbe9e6a575aec6fb3a47cfdfd0d5
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-16 12:59:43 +0300

    Implement MongoDB posts CRUD

--- migrant-hub-backend/controllers/postController.js
+++ migrant-hub-backend/controllers/postController.js
@@ -1,24 +1,102 @@
-const getAllPosts = (req, res) => {
-  res.json([]);
+const Post = require("../models/postModel");
+
+// GET all posts
+const getAllPosts = async (req, res, next) => {
+  try {
+    const filter = {};
+
+    if (req.query.category) {
+      filter.category = req.query.category;
+    }
+
+    if (req.query.communityId) {
+      filter.communityId = req.query.communityId;
+    }
+
+    if (req.query.search) {
+      filter.$or = [
+        { title: { $regex: req.query.search, $options: "i" } },
+        { body: { $regex: req.query.search, $options: "i" } }
+      ];
+    }
+
+    const posts = await Post.find(filter)
+      .sort({ createdAt: -1 })
+      .limit(Number(req.query.limit) || 0);
+
+    res.status(200).json(posts);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getPostById = (req, res) => {
-  res.json({ id: req.params.id });
+// GET one post
+const getPostById = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        error: "Not found"
+      });
+    }
+
+    res.status(200).json(post);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const createPost = (req, res) => {
-  res.status(201).json(req.body);
+// POST
+const createPost = async (req, res, next) => {
+  try {
+    const post = await Post.create(req.body);
+
+    res.status(201).json(post);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updatePost = (req, res) => {
-  res.json({
-    id: req.params.id,
-    ...req.body
-  });
+// PATCH
+const updatePost = async (req, res, next) => {
+  try {
+    const post = await Post.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    if (!post) {
+      return res.status(404).json({
+        error: "Not found"
+      });
+    }
+
+    res.status(200).json(post);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deletePost = (req, res) => {
-  res.status(204).send();
+// DELETE
+const deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        error: "Not found"
+      });
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
